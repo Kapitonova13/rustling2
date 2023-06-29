dfd08e13bec8eda37e61e0d11adf74412705650b
Anna <anyakapitosha@yandex.ru>
2023-06-30 03:46:33 +0500

    rustlings 1 (quiz)

--- rustlings/exercises/quiz1.rs
+++ rustlings/exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(pr: u32) -> u32 {
+    if pr > 40 {
+        pr * 1
+    } else {
+        pr * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
